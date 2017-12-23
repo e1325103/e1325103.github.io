5db9ebf417dd86c418a57f37fa731aef3041c3bb
1325103 <e1325103@student.tuwien.ac.at>
2017-12-23 17:43:22 +0100

    Space Interpolation Working

--- InfoVis/GeoTimeContour/TimeContour.js
+++ InfoVis/GeoTimeContour/TimeContour.js
@@ -1,8 +1,12 @@
-﻿var rendererSpace;
-var rendererTime = [];
+﻿
 
 // 2D draw area, with margin for axis labels
 var margin = { top: 20, right: 20, bottom: 20, left: 20 };
+
+const TIME_DATA_WIDTH = 959;
+const TIME_DATA_HEIGHT = 37;
+const SPACE_DATA_WIDTH = 152;
+const SPACE_DATA_HEIGHT = 137;
 
 // related to threejs
 var dataTexture;
@@ -10,108 +14,33 @@ var material;
 var scene, camera;
 var geometry, mesh;
 
-var done = false;
+var rendererSpace;
+var rendererTime = [];
+
+var sceneSpace;
+var sceneTime = [];
+var cameraSpace;
+var cameraTime;
+
 var dayIndex = [];
+var dayBuffer = [];
 
-function generateRenderer(index, year, width, height) {
+var pDataTexture;
+var nDataTexture;
+var interpDataTexture;
 
-    width = width - margin.left - margin.right;
-    height = height - margin.top - margin.bottom;
-
-    // linear mapping of X random variable points of interest to draw area x axis
-    //var x = d3.scale.linear()
-    //    .domain([0, 365])
-    //    .range([0, width]);
-
-    //// linear mapping of Y random variable points of interest to draw area y axis
-    //var y = d3.scale.linear()
-    //    .domain([0, 1])
-    //    .range([height, 0]);
-
-    //// svg container for axes
-    //d3.select("svg").remove(); // clear existing svg container
-    //var domDiv = "#timeContourDiv" + index;
-    //if (index < 0) {
-    //    domDiv = "#spaceContourDivI";
-    //}
-    //var svg = d3.select(domDiv) // return DOM object (related to html element) body
-    //    .append("svg") // in body object create new svg object and return it
-    //    .attr("width", width + margin.left + margin.right) // set svg attr and return itself
-    //    .attr("height", height + margin.top + margin.bottom)
-    //    .append("g") // in svg object create group of svg shapes and return it
-    //    .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
-    //    .on("mousemove", mousemove); // etc.
-
-    //svg.append("g").append("rect")
-    //    .attr("fill", "pink")
-    //    .attr("height", "100%")
-    //    .attr("width", "100%");
-
-    //// d3 axes
-    //var xAxis = d3.svg.axis()
-    //    .scale(x) // scale axis to range x
-    //    .orient("bottom");
-
-    //var yAxis = d3.svg.axis()
-    //    .scale(y)
-    //    .orient("left");
-
-    //// draw x axis
-    //// note on "call" function:
-    //// below xAxis is a function returned by axis().
-    //// <whatever>.call(xAxis) is same as xAxis(<whatever>),
-    //// i.e. run xAxis() function on <whatever>, here on the svg group g object.
-    //svg.append("g") // group of svg shapes
-    //    .attr("class", "x axis")
-    //    .attr("transform", "translate(0," + height + ")")
-    //    .attr("fill", "#888")
-    //    .call(xAxis)
-    //    .append("text")
-    //    .attr("class", "label")
-    //    .attr("x", width)
-    //    .attr("y", 42)
-    //    .style("text-anchor", "end")
-    //    .text("departure delay (mins)");
-
-    //// draw y axis
-    //svg.append("g")
-    //    .attr("class", "y axis")
-    //    .attr("fill", "#888")
-    //    .call(yAxis)
-    //    .append("text")
-    //    .attr("class", "label")
-    //    .attr("transform", "rotate(-90)")
-    //    .attr("y", -60)
-    //    .attr("dy", ".71em")
-    //    .style("text-anchor", "end")
-    //    .text("arrival delay (mins)");
-
-    geometry = new THREE.PlaneGeometry(width, height, 1, 1);
-
-
-    material = new THREE.ShaderMaterial({
-        vertexShader: document.getElementById('vertexShader').textContent,
-        fragmentShader: document.getElementById('fragmentShader').textContent
-    });
-
+function updateTime(index, year) {
     var filePath = "Year/" + ("" + year) + ".csv";
-    var dataWidth = 959;
-    var dataHeight = 37;
-
-    if (index < 0) {
-        filePath = "Day/2016_20.csv";
-        dataWidth = 152;
-        dataHeight = 137;
-    }
 
     d3.csv(filePath, function (error, data) {
+
         var textData = [];
 
-        for (var i = 0; i < (dataWidth * dataHeight); i++) {
+        for (var i = 0; i < (TIME_DATA_WIDTH * TIME_DATA_HEIGHT); i++) {
             textData.push(+data[i].V);
         }
 
-        dataTexture = new THREE.DataTexture(new Float32Array(textData), dataWidth, dataHeight, THREE.LuminanceFormat, THREE.FloatType);
+        dataTexture = new THREE.DataTexture(new Float32Array(textData), TIME_DATA_WIDTH, TIME_DATA_HEIGHT, THREE.LuminanceFormat, THREE.FloatType);
         dataTexture.wrapS = THREE.ClampToEdgeWrapping;
         dataTexture.wrapT = THREE.ClampToEdgeWrapping;
         dataTexture.magFilter = THREE.LinearFilter;
@@ -125,28 +54,100 @@ function generateRenderer(index, year, width, height) {
             }
         };
 
-        material.uniforms = uniform;
-        material.needsUpdate = true;
-        mesh.needsUpdate = true;
-        if (index >= 0) {
-            rendererTime[index].render(scene, camera);
-        } else {
-            rendererSpace.render(scene, camera);
-        }
-        console.log("Load Complete");
-        done = true;
+        sceneTime[index].children[0].material.uniforms = uniform;
+        sceneTime[index].children[0].material.needsUpdate = true;
+        sceneTime[index].children[0].needsUpdate = true;
+        rendererTime[index].render(sceneTime[index], cameraTime);
     });
+}
+
+function updateSpace(time) {
+    interpDataTexture = time[2][1];
+
+    var textData = [];
+
+    for (var i = 0; i < (SPACE_DATA_WIDTH * SPACE_DATA_HEIGHT); i++) {
+        textData.push(+dayBuffer[time[0][0]][dayIndex[time[0][0]][time[0][1]]][i]);
+    }
+
+    pDataTexture = new THREE.DataTexture(new Float32Array(textData), SPACE_DATA_WIDTH, SPACE_DATA_HEIGHT, THREE.LuminanceFormat, THREE.FloatType);
+    pDataTexture.wrapS = THREE.ClampToEdgeWrapping;
+    pDataTexture.wrapT = THREE.ClampToEdgeWrapping;
+    pDataTexture.magFilter = THREE.LinearFilter;
+    pDataTexture.minFilter = THREE.LinearFilter;
+    pDataTexture.generateMipmaps = false;
+    pDataTexture.repeat.set(1, 1);
+    pDataTexture.needsUpdate = true;
+
+    var textData = [];
+
+    for (var i = 0; i < (SPACE_DATA_WIDTH * SPACE_DATA_HEIGHT); i++) {
+        textData.push(+dayBuffer[time[1][0]][dayIndex[time[1][0]][time[1][1]]][i]);
+    }
+
+    nDataTexture = new THREE.DataTexture(new Float32Array(textData), SPACE_DATA_WIDTH, SPACE_DATA_HEIGHT, THREE.LuminanceFormat, THREE.FloatType);
+    nDataTexture.wrapS = THREE.ClampToEdgeWrapping;
+    nDataTexture.wrapT = THREE.ClampToEdgeWrapping;
+    nDataTexture.generateMipmaps = false;
+    nDataTexture.magFilter = THREE.LinearFilter;
+    nDataTexture.minFilter = THREE.LinearFilter;
+    nDataTexture.repeat.set(1, 1);
+    nDataTexture.needsUpdate = true;
+
+    uniform = {
+        pMap: {
+            value: pDataTexture
+        },
+        nMap: {
+            value: nDataTexture
+        },
+        d: {
+            value: interpDataTexture
+        }
+    };
+
+    var material = new THREE.ShaderMaterial({
+        vertexShader: document.getElementById('vertexShader').textContent,
+        fragmentShader: document.getElementById('fragmentShaderSpace').textContent
+    });
+    material.uniforms = uniform;
+    material.needsUpdate = true;
+
+    sceneSpace.children[0].material = material;
+    sceneSpace.children[0].needsUpdate = true;
+    console.log("RENDER");
+    rendererSpace.render(sceneSpace, cameraSpace);
+}
+
+function generateRenderer(index, year, width, height) {
+
+    width = width - margin.left - margin.right;
+    height = height - margin.top - margin.bottom;
+
+    var geometry = new THREE.PlaneGeometry(width, height, 1, 1);
+    var material;
+    if (index >= 0) {
+        material = new THREE.ShaderMaterial({
+            vertexShader: document.getElementById('vertexShader').textContent,
+            fragmentShader: document.getElementById('fragmentShaderTime').textContent
+        });
+    } else {
+        material = new THREE.ShaderMaterial({
+            vertexShader: document.getElementById('vertexShader').textContent,
+            fragmentShader: document.getElementById('fragmentShaderTime').textContent
+        });
+    }
 
     // mesh
-    mesh = new THREE.Mesh(geometry, material);
+    var mesh = new THREE.Mesh(geometry, material);
 
     // scene
-    scene = new THREE.Scene();
+    var scene = new THREE.Scene();
     scene.add(mesh);
 
     // ortho camera
     // clipping plane distances: left, right, top, bottom, near, far
-    camera = new THREE.OrthographicCamera(-width / 2, width / 2, height / 2, -height / 2, 1, 1000);
+    var camera = new THREE.OrthographicCamera(-width / 2, width / 2, height / 2, -height / 2, 1, 1000);
     camera.position.z = 1000;
 
     var renderer = new THREE.WebGLRenderer();
@@ -159,8 +160,12 @@ function generateRenderer(index, year, width, height) {
 
     if (index >= 0) {
         rendererTime[index] = renderer;
+        sceneTime[index] = scene;
+        cameraTime = camera;
     } else {
         rendererSpace = renderer;
+        sceneSpace = scene;
+        cameraSpace = camera;
     }
 }
 
@@ -183,7 +188,34 @@ function init() {
             }
             dayIndex[+data[i].Y].push(+data[i].D);
         }
+        initYear(2014);
+        initYear(2015);
+        initYear(2016);
+        updateTime(0, 2014);
+        updateTime(1, 2015);
+        updateTime(2, 2016);
+        var time = findNextDate(2014, 125);
+        updateSpace(time);
     });
+
+}
+
+function initYear(year) {
+    if ((typeof dayBuffer[year] == "undefined") || (dayBuffer[year].length <= 0)) {
+        dayBuffer[year] = [];
+        for (var i = 0; i < dayIndex[year].length; i++) {
+            (function (day) {
+                d3.csv("Day/" + year + "_" + dayIndex[year][day] + ".csv", function (error, data) {
+                    var textData = [];
+
+                    for (var j = 0; j < (SPACE_DATA_WIDTH * SPACE_DATA_HEIGHT); j++) {
+                        textData.push(+data[j].V);
+                    }
+                    dayBuffer[year][dayIndex[year][day]] = textData;
+                });
+            })(i);
+        }
+    }
 }
 
 function mousemove(e) {
@@ -193,11 +225,12 @@ function mousemove(e) {
         var y = (e.clientY - $("#" + e.target.id).position().top) / $("#" + e.target.id).height();
         y = 1.0 - Math.min(Math.max(y, 0.0), 1.0);
 
-        console.log("x: " + x + " - y: " + y);
+        //console.log("x: " + x + " - y: " + y);
 
         if (e.target.id.includes("time")) {
             var index = +e.target.id.substring(e.target.id.length - 1);
-            findNextDate(2014 + index, x * 365);
+            var time = findNextDate(2014 + index, x * 365);
+            updateSpace(time);
         }
     }
 }
@@ -209,51 +242,23 @@ function findNextDate(year, day) {
     var pYear = year;
     for (nDay = 0; nDay < dayIndex[year].length && dayIndex[year][nDay] <= day; nDay++);
     pDay = nDay - 1;
+    var d = (day - dayIndex[pYear][pDay]) / (dayIndex[nYear][nDay] - dayIndex[pYear][pDay]);
     if (nDay == dayIndex[year].length) {
         pDay = dayIndex[year].length - 1;
         nDay = 1;
         nYear = nYear + 1;
+        d = (day - dayIndex[pYear][pDay]) / (366 - dayIndex[pYear][pDay]);
     } else if (nDay == 0) {
         pDay = dayIndex[year - 1].length - 1;
         pYear = pYear - 1;
+        d = (day + 1) / (dayIndex[nYear][nDay] + 1);
     }
-    console.log(pYear + " " + dayIndex[pYear][pDay] + " <> " + year + " " + Math.round(day) + " <> " + nYear + " " + dayIndex[nYear][nDay]);
-}
-
-function update(index) {
-    d3.csv("Year/2015.csv", function (error, data) {
-        var textData = [];
-
-        for (var i = 0; i < (959 * 37); i++) {
-            textData.push(+data[i].V);
-        }
-
-        dataTexture = new THREE.DataTexture(new Float32Array(textData), 959, 37, THREE.LuminanceFormat, THREE.FloatType);
-        dataTexture.wrapS = THREE.ClampToEdgeWrapping;
-        dataTexture.wrapT = THREE.ClampToEdgeWrapping;
-        dataTexture.magFilter = THREE.LinearFilter;
-        dataTexture.minFilter = THREE.LinearFilter;
-        dataTexture.repeat.set(1, 1);
-        dataTexture.needsUpdate = true;
-
-
-        material = new THREE.ShaderMaterial({
-            vertexShader: document.getElementById('vertexShader').textContent,
-            fragmentShader: document.getElementById('fragmentShader').textContent
-        });
-        uniform = {
-            map: {
-                value: dataTexture
-            }
-        };
-        material.uniforms = uniform;
-        scene.children[0].material = material;
-        scene.children[0].material.needsUpdate = true;
-        scene.children[0].needsUpdate = true;
-        scene.needsUpdate = true;
-        rendererTime[index].render(scene, camera);
-        console.log("UPDATE");
-    });
+    console.log(pYear + " " + dayIndex[pYear][pDay] + " <" + (1 - d) + "> " + year + " " + Math.round(day) + " <" + d + "> " + nYear + " " + dayIndex[nYear][nDay]);
+    var result = [];
+    result[0] = [pYear, pDay];
+    result[1] = [nYear, nDay];
+    result[2] = [(1 - d), d];
+    return result;
 }
 
 window.addEventListener("load", init, false);
